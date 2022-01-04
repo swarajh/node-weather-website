4ec6bf902fef34065f8b78429f5f0432da3485d0
Swaraj Halikar <swaraj@Swarajs-MacBook-Air.local>
2022-01-04 11:37:49 +0530

    Add high and low to forecast

--- src/utils/weather.js
+++ src/utils/weather.js
@@ -1,7 +1,7 @@
 const request=require('request')
 
 const weather=(latitude, longitude, callback)=>{
-     const url ='http://api.weatherstack.com/current?access_key=c58c304b21171c34278e2fdb4c509cce&query=. '+ latitude + ',' + longitude + '&units=f'
+     const url ='http://api.weatherstack.com/current?access_key=c58c304b21171c34278e2fdb4c509cce&query=. '+ latitude + ',' + longitude
      request({url, json:true },(error,{body}={})=>{
         if(error){
                     callback('Unable to connect')
@@ -10,7 +10,7 @@ const weather=(latitude, longitude, callback)=>{
             
                      callback('Unable to find location')
                  }
-                 else{   callback(undefined,body.current.weather_descriptions+ '. It is currently ' +body.current.temperature+ ' degrees out. There is a  '+body.current.precip+' % chance of rain')
+                 else{   callback(undefined,'Sky is '+body.current.weather_descriptions+ '. It is currently ' +body.current.temperature+ ' degrees celsius out. The humidity is '+body.current.humidity+'%.')
             
             
                  }
