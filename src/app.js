2f11c75d961b19337e79e54aef06df9d302a58a0
Swaraj Halikar <swaraj@Swarajs-MacBook-Air.local>
2022-01-04 10:25:53 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const weather = require('./utils/weather')
 const path = require('path')
 const express = require('express')
 const app = express()
+const port =process.env.PORT || 3000
 const hbs = require('hbs')
 const { error } = require('console')
 const { response } = require('express')
@@ -97,8 +98,8 @@ app.get('*', (req, res) => {
     })
 
 })
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' +port)
 
 })
 
